4c35962cf34970628a32bcfa7e2100718c30762f
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-10-13 20:57:17 +0800

    Also wrtie log to file

--- server/src/infra/logger.rs
+++ server/src/infra/logger.rs
@@ -1,12 +1,17 @@
+use std::fs::File;
+
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::filter::ParseError;
 use tracing_subscriber::fmt::time::ChronoLocal;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 pub struct Logger {}
 
 impl Logger {
     pub fn init() {
+        let file = File::create("logs.txt").expect("Failed to create log file");
+        let writer = file.and(std::io::stdout);
         let filter: Result<EnvFilter, ParseError> = try {
             EnvFilter::builder()
                 .with_default_directive(LevelFilter::DEBUG.into())
@@ -19,6 +24,7 @@ impl Logger {
 
         tracing_subscriber::fmt()
             .with_timer(ChronoLocal::new("%Y-%m-%d %H:%M:%S".to_string()))
+            .with_writer(writer)
             .with_env_filter(filter)
             // .with_max_level(tracing::Level::DEBUG)
             .init();
