55ff94bebda9231b06737370a776c0d30209a4c5
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2025-12-25 18:03:25 +0800

    Update flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Touhou Cloud DB dev env (root)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+          ];
+        };
+      }
+    ));
+}
+
