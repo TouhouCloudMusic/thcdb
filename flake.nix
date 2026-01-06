48f6ecb6683b5e977690e48e7f1b711294f82f9f
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2026-01-06 23:52:37 +0800

    Refactor flakes

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
@@ -11,6 +15,7 @@
   outputs =
     {
       self,
+      fenix,
       nixpkgs,
       flake-utils,
     }:
@@ -19,15 +24,52 @@
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [
+            fenix.overlays.default
+          ];
+        };
+
+        schemathesis = pkgs.buildFHSEnv {
+          name = "schemathesis";
+          targetPkgs =
+            pkgs': with pkgs'; [
+              python3
+              uv
+            ];
+          runScript = pkgs.writeShellScript "schemathesis-fhs" ''
+            export UV_NO_MANAGED_PYTHON=1
+            exec uvx schemathesis "$@"
+          '';
         };
       in
       {
         devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc-codegen-cranelift-preview"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
+            clang
+            mold
+            openssl
+            pkg-config
+            schemathesis
+          ];
           packages = with pkgs; [
+            dprint
             just
+            sea-orm-cli
+            just-lsp
+            nodejs_22
+            typescript-go
+            pnpm
+            oxlint
           ];
         };
       }
     ));
 }
-
