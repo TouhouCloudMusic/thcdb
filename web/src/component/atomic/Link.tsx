2505c6f9ee7492e797716ac7edb504b3f1de4fbc
Huliiiiii <308013446a@gmail.com>
2026-02-12 03:15:03 +0800

    Add underline prop to control underline display of Link

--- web/src/component/atomic/Link.tsx
+++ web/src/component/atomic/Link.tsx
@@ -2,13 +2,21 @@ import type { LinkComponentProps } from "@tanstack/solid-router"
 import { Link as RouterLink } from "@tanstack/solid-router"
 import { twMerge } from "tailwind-merge"
 
-const LINK_CLASS =
-	"text-primary underline-offset-4 transition-colors hover:underline"
+const LINK_CLASS = "text-primary underline-offset-4 transition-colors"
 
-export type LinkProps = LinkComponentProps<"a">
+export type LinkProps = LinkComponentProps<"a"> & {
+	/**
+	 * Whether the underline shows on hover.
+	 * Default: `true`
+	 */
+	underline?: boolean
+}
 
 export function Link(props: LinkProps) {
-	const className = () => twMerge(LINK_CLASS, props.class)
+	const className = () => {
+		const underline = props.underline ?? true
+		return twMerge(LINK_CLASS, underline && "hover:underline", props.class)
+	}
 
 	return (
 		<RouterLink
