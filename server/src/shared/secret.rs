0c019af0379e0adb118d9bc1e3a3906e0f54a0c5
Huliiiiii <308013446a@gmail.com>
2026-03-10 14:12:20 +0800

    Add tests for secret module

--- server/src/shared/secret.rs
+++ server/src/shared/secret.rs
@@ -56,3 +56,38 @@ pub fn verify(
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{hash, verify};
+
+    #[tokio::test]
+    async fn verify_returns_true_if_matches() {
+        let plaintext = "Password123123!";
+        let hashed = hash(plaintext).await.unwrap();
+
+        let is_valid =
+            verify(hashed, plaintext.as_bytes().to_vec()).await.unwrap();
+
+        assert!(is_valid);
+    }
+
+    #[tokio::test]
+    async fn verify_returns_false_for_non_matching_secret() {
+        let hashed = hash("Password123123!").await.unwrap();
+
+        let is_valid =
+            verify(hashed, b"wrong-password".to_vec()).await.unwrap();
+
+        assert!(!is_valid);
+    }
+
+    #[tokio::test]
+    async fn verify_returns_error_for_invalid_hash() {
+        let result =
+            verify("invalid-hash".to_string(), b"Password123123!".to_vec())
+                .await;
+
+        assert!(result.is_err());
+    }
+}
